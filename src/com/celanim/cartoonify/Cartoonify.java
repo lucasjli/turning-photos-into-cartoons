640e8bcc8952288e4a213c07e143b65deb62463d
lucas <lijie.lucas@hotmail.com>
2025-04-03 00:59:53 +1300

    Remove needless java.util.stream.IntStream

--- src/com/celanim/cartoonify/Cartoonify.java
+++ src/com/celanim/cartoonify/Cartoonify.java
@@ -6,7 +6,6 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.stream.IntStream;
 
 import javax.imageio.ImageIO;
 
